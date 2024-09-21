e496c5065b4003c95bb52274f03514725bce8f16
Shiwon Park <psw041115@gmail.com>
2024-09-21 22:19:27 +0900

    [Feature]Use Fastify Fixes #18

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import expressBasicAuth from 'express-basic-auth';
 import { ConfigService } from '@nestjs/config';
-import cookieParser from 'cookie-parser';
 import { ExceptionLogFilter } from './global/filter/exceptionLog.filter';
+import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const adapter = new FastifyAdapter();
+  // CORS 설정
+  const whitelist = [/https:\/\/.*gistory.me/, /http:\/\/localhost:3000/];
+  adapter.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.some((regex) => regex.test(origin))) {
+        callback(null, origin);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  // cookie 설정
+  await app.register(fastifyCookie);
   // Swagger 보안 설정
   const configService = app.get(ConfigService);
   app.use(
@@ -24,7 +49,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Infoteam-Idp API Docs')
     .setDescription('인포팀 idp의 API 문서입니다.')
-    .setVersion('2024-04')
+    .setVersion(configService.getOrThrow<string>('API_VERSION'))
     .addBearerAuth(
       {
         type: 'http',
@@ -48,24 +73,9 @@ async function bootstrap() {
       displayRequestDuration: true,
     },
   });
-  // Cookie 설정
-  app.use(cookieParser());
-  // CORS 설정
-  const whitelist = [/https:\/\/.*gistory.me/, /http:\/\/localhost:3000/];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || whitelist.some((regex) => regex.test(origin))) {
-        callback(null, origin);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
+  // 글로벌 예외처리
   app.useGlobalFilters(new ExceptionLogFilter());
-  await app.listen(3000);
+  // 서버 실행
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
