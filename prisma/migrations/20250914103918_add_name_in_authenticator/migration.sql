ef7dbca73e95afcde8bbc56e293f98413aee6b8c
Dohyun Kim <dohyun06b@gmail.com>
2025-09-15 15:16:12 +0900

    fix: add unique to credential id credential id가 unique한 애는 아닌데 패스키 찾으려면 unique 해야함...

--- prisma/migrations/20250914103918_add_name_in_authenticator/migration.sql
+++ prisma/migrations/20250914103918_add_name_in_authenticator/migration.sql
@@ -4,8 +4,5 @@
   - Added the required column `name` to the `authenticator` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropIndex
-DROP INDEX "authenticator_credential_id_key";
-
 -- AlterTable
 ALTER TABLE "authenticator" ADD COLUMN     "name" TEXT NOT NULL;
