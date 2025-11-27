23c45a6d890b5644a99ba54341a92752019fca96
2paperstar <lee@paperst.ar>
2025-11-27 10:39:41 +0900

    feat: add templates module

---
+++ libs/templates/src/templates.module.md
@@ -0,0 +1,12 @@
+# Templates Module
+
+메일 템플릿을 관리하는 모듈입니다
+
+## Environment
+
+사용되는 package들은 다음과 같습니다.
+
+- react-email
+- @react-email/components
+- react
+- react-dom
---
+++ libs/templates/src/templates/password.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import Email from './email';
+
+const Password = ({ password }: { password: string }): React.ReactNode => {
+  return (
+    <Email
+      title="인포팀 계정 임시 비밀번호"
+      preview="인포팀 계정 임시 비밀번호"
+      code={password}
+      description={
+        <>
+          <span className="text-primary-600 font-bold">인포팀 계정</span> 이메일
+          임시 비밀번호 전송용 메일입니다.
+          <br />
+          상기 임시 비밀번호를 입력하여 로그인을 완료해주세요.
+          <br />
+          <br />
+          <span className="font-bold">중요: </span>로그인 후 꼭 비밀번호 변경을
+          하여 임시 비밀번호를 제거하세요.
+        </>
+      }
+    />
+  );
+};
+
+export default Password;
